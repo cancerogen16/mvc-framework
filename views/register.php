1b9dce00e710686403472c2c411b200b3908f139
cancerogen16 <don544@mail.ru>
2022-03-31 02:42:36 +0500

    alert alert-danger

--- views/register.php
+++ views/register.php
@@ -1,33 +1,61 @@
 <h1>Registration page</h1>
 
-<?php if (!empty($errors)) : ?>
-<?php foreach ($errors as $error) : ?>
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <?= $error ?>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-<?php endforeach; ?>
-<?php endif; ?>
-
 <form action="" method="post">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="text"
+               class="form-control<?php echo $model->hasError('name') ? ' is-invalid' : '' ?>"
+               id="name" name="name" aria-describedby="nameHelp">
+        <?php if (!empty($model->errors['name'])) : ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?php foreach ($model->errors['name'] as $error) : ?>
+                    <p><?= $error ?></p>
+                <?php endforeach; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="text" class="form-control" id="email" name="email" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="text"
+               class="form-control<?php echo $model->hasError('email') ? ' is-invalid' : '' ?>"
+               id="email" name="email" aria-describedby="emailHelp">
+        <?php if (!empty($model->errors['email'])) : ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <?php foreach ($model->errors['email'] as $error) : ?>
+                <p><?= $error ?></p>
+            <?php endforeach; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php endif; ?>
     </div>
     <div class="mb-3">
         <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="password"
+               class="form-control<?php echo $model->hasError('password') ? ' is-invalid' : '' ?>"
+               id="password" name="password" aria-describedby="passwordHelp">
+        <?php if (!empty($model->errors['password'])) : ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?php foreach ($model->errors['password'] as $error) : ?>
+                    <p><?= $error ?></p>
+                <?php endforeach; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
     </div>
     <div class="mb-3">
         <label for="confirm" class="form-label">Confirm password</label>
-        <input type="password" class="form-control" id="confirm" name="confirm">
+        <input type="password"
+               class="form-control<?php echo $model->hasError('confirm') ? ' is-invalid' : '' ?>"
+               id="confirm" name="confirm" aria-describedby="confirmHelp">
+        <?php if (!empty($model->errors['confirm'])) : ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?php foreach ($model->errors['confirm'] as $error) : ?>
+                    <p><?= $error ?></p>
+                <?php endforeach; ?>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
