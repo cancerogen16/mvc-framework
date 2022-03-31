313c6457db93e7a20d425e4f152c7bd064f6d292
cancerogen16 <don544@mail.ru>
2022-04-01 01:38:03 +0500

    m0002_add_password_column

---
+++ database/migrations/m0002_add_password_column.php
@@ -0,0 +1,22 @@
+<?php
+
+class m0002_add_password_column
+{
+    public function up()
+    {
+        $db = \App\Core\Application::$app->db;
+
+        $SQL = "ALTER TABLE users ADD COLUMN password VARCHAR(255) NOT NULL;";
+
+        $db->pdo->exec($SQL);
+    }
+
+    public function down()
+    {
+        $db = \App\Core\Application::$app->db;
+
+        $SQL = "ALTER TABLE users DROP COLUMN password;";
+
+        $db->pdo->exec($SQL);
+    }
+}
