ffff15a344e382b54dd3b1e1d040f9fdf6484f3a
cancerogen16 <don544@mail.ru>
2022-04-01 01:34:07 +0500

    users table

---
+++ database/migrations/m0001_initial.php
@@ -0,0 +1,28 @@
+<?php
+
+class m0001_initial
+{
+    public function up()
+    {
+        $db = \App\Core\Application::$app->db;
+
+        $SQL = "CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            status TINYINT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ) ENGINE=INNODB;";
+
+        $db->pdo->exec($SQL);
+    }
+
+    public function down()
+    {
+        $db = \App\Core\Application::$app->db;
+
+        $SQL = "DROP TABLE users;";
+
+        $db->pdo->exec($SQL);
+    }
+}
