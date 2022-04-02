7bb80be602e0832d65d73499ca0a520da9077e63
cancerogen16 <don544@mail.ru>
2022-04-02 22:19:48 +0500

    lowercase folder names

---
+++ controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\controllers;
+
+use erast\phpmvc\Application;
+use erast\phpmvc\Middlewares\AuthMiddleWare;
+use erast\phpmvc\Request;
+use erast\phpmvc\Response;
+use App\models\LoginForm;
+use App\models\User;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->registerMiddleWare(new AuthMiddleWare(['profile']));
+    }
+
+    public function login(Request $request, Response $response)
+    {
+        $loginForm = new LoginForm();
+
+        if ($request->isPost()) {
+            $loginForm->loadData($request->getBody());
+
+            if ($loginForm->validate() && $loginForm->login()) {
+                Application::$app->session->setFlash('success', 'Welcome!');
+
+                $response->redirect('/');
+            }
+        }
+
+        $this->setLayout('auth');
+
+        return $this->render('login', [
+            'model' => $loginForm,
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $user = new User;
+
+        if ($request->isPost()) {
+            $user->loadData($request->getBody());
+
+            if ($user->validate() && $user->save()) {
+                Application::$app->session->setFlash('success', 'Thanks for registering');
+
+                Application::$app->response->redirect('/');
+            }
+
+            return $this->render('register', [
+                'model' => $user,
+            ]);
+        }
+
+        $this->setLayout('auth');
+
+        return $this->render('register', [
+            'model' => $user,
+        ]);
+    }
+
+    public function logout(Request $request, Response $response)
+    {
+        Application::$app->logout();
+
+        $response->redirect('/');
+    }
+
+    public function profile()
+    {
+        return $this->render('profile');
+    }
+}
