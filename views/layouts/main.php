408af68712c4b23a25cdc57b3668af2c0a859b03
cancerogen16 <don544@mail.ru>
2022-04-02 23:11:47 +0500

    layouts styles

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -6,8 +6,7 @@ use erast\phpmvc\Application;
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="/css/cosmo/bootstrap.min.css">
     <title><?= $this->title ?></title>
 </head>
 <body>
